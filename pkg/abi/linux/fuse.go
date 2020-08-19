e5afb594201b519c7917f79c3ee3dc5757abf9f1
boyuan-he <67342292+boyuan-he@users.noreply.github.com>
2020-08-19 16:48:42 -0700

    Revert "Revert "fuse: Fuse read patch""

--- pkg/abi/linux/fuse.go
+++ pkg/abi/linux/fuse.go
@@ -189,10 +189,9 @@ const (
 
 // Constants relevant to FUSE operations.
 const (
-	FUSE_NAME_MAX       = 1024
-	FUSE_PAGE_SIZE      = 4096
-	FUSE_DIRENT_ALIGN   = 8
-	FUSE_READ_LOCKOWNER = 1 << 1
+	FUSE_NAME_MAX     = 1024
+	FUSE_PAGE_SIZE    = 4096
+	FUSE_DIRENT_ALIGN = 8
 )
 
 // FUSEInitIn is the request sent by the kernel to the daemon,
@@ -405,6 +404,11 @@ type FUSEOpenOut struct {
 	OpenFlag uint32
 }
 
+// FUSE_READ flags, consistent with the ones in include/uapi/linux/fuse.h.
+const (
+	FUSE_READ_LOCKOWNER = 1 << 1
+)
+
 // FUSEReadIn is the request sent by the kernel to the daemon
 // for FUSE_READ.
 //
@@ -426,7 +430,7 @@ type FUSEReadIn struct {
 	// LockOwner is the id of the lock owner if there is one.
 	LockOwner uint64
 
-	// Flags for the underling file.
+	// Flags for the underlying file.
 	Flags uint32
 
 	_ uint32
